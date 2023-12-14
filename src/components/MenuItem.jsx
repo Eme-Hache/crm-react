16f6f3db8222057feffbf8664e36b7ba2ec2856e
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-14 16:30:04 -0500

    fix: menu item text fixed

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -8,14 +8,14 @@ const MenuItem = ({ to, icon, toggled, children }) => {
         <Link
             to={to}
             className={`${to === currentURL && 'bg-blue-700 text-gray-50 shadow-lg'} 
-                text-md mt-4 transition duration-200
-                px-5 rounded-md text-gray-600
+                text-md transition duration-200
+                px-5 rounded-md
                 hover:bg-blue-700 hover:text-white 
-                hover:shadow-lg flex items-center overflow-hidden h-16`}
+                hover:shadow-lg flex items-center overflow-hidden h-16 w-full`}
         >
             <i className={`${icon} mr-6 text-xl`} />
 
-            <span className={`${toggled ? 'opacity-0' : 'opacity-100'} whitespace-nowrap transition-all`}>
+            <span className={`${toggled ? 'opacity-0' : 'opacity-100'} whitespace-nowrap`}>
                 {children}
             </span>
         </Link>
